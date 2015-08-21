f335e2967e506b99e9df1fe3e070ab6133988df1
Fan Jiang <fjjiang@amazon.com>
2015-08-21 10:33:28 +0800

    coding daily version 0.1

---
+++ c++/array_and_string/array.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    // static array
+    int arr[15];
+    int n = 0;
+    int value = 0;
+    cout << "Enter values. Type \"-1\" to stop: ";
+    while (n < 15 && value != -1){
+        cin >> value;
+        if (value != -1){
+            arr[n] = value;
+            n++;
+        }
+    }
+    if (n == 0){
+        cout << "You have entered no value,bye!"
+    }else{
+        int min = arr[0];
+        for (int i=1; i<n; i++){
+            if (arr[i] < min){
+                min = arr[i];
+            }
+        }
+        cout << "The minimal value of the array is " << min;
+    }
+    return 0;
+}
+
